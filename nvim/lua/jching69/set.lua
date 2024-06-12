1b7421f828d105fa90843c72605ea88cab5a0a8d
j-ching69 <ocezask@hotmail.com>
2024-06-13 07:42:28 +0800

    neovim: change tab to 4 spaces;

--- nvim/lua/jching69/set.lua
+++ nvim/lua/jching69/set.lua
@@ -1,8 +1,8 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 3
-vim.opt.softtabstop = 3
-vim.opt.shiftwidth = 3
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
